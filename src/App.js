ec3c1d60081f73af440d0fe14cc2959695820c71
Nagindra <nagi@Nagindras-MacBook-Air.local>
2019-07-05 12:35:14 +0200

    phoneBook netlify setup

---
+++ src/App.js
@@ -0,0 +1,13 @@
+// App.js
+import React, { Component } from 'react'
+import './App.css'
+import ProductTable from './components/ProductTable'
+export default class App extends Component {
+  render() {
+    return (
+      <div>
+        <ProductTable />
+      </div>
+    )
+  }
+}
